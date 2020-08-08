86e54b427fc74970b219920a9598b154908d6c76
MrBoog1e <68776771+MrBoog1e@users.noreply.github.com>
2020-08-08 15:43:20 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -4,6 +4,6 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_checking_the_stores_button (browser):
     browser.get(link)
-    button = browser.find_element_by_css_selector('button.btn')
+    button = browser.find_element_by_css_selector('#add_to_basket_form > button')
     assert button, 'The button is missing :('
 
