c6373af008ead2ea44e6aff3d00e3dfeb9849426
Dmitry Zbirko <mrboog1e@ya.ru>
2020-08-09 16:09:09 +0300

    внёс поправки в assert

--- test_items.py
+++ test_items.py
@@ -4,6 +4,6 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_checking_the_stores_button (browser):
     browser.get(link)
-    button = browser.find_element_by_css_selector('#add_to_basket_form > button')
-    assert button, 'The button is missing :('
+    addToCartButton = browser.find_elements_by_css_selector(".add-to-basket .btn")
+    assert len(addToCartButton) > 0, 'There is no add to cart button'
 
